ae6436d111ab2e5a486e26d0d25d00158a85496c
Jakob van Santen <jvansanten@gmail.com>
2025-10-16 16:34:57 +0200

    Remove unused extract_alert

--- ampel/lsst/archive/server/cutouts.py
+++ ampel/lsst/archive/server/cutouts.py
@@ -1,7 +1,6 @@
 from typing import Any, BinaryIO
 
 import fastavro
-from fastavro._read_py import BLOCK_READERS, BinaryDecoder
 
 ALERT_SCHEMAS: dict[str, Any] = {}
 
@@ -17,27 +16,3 @@ def read_schema(fo: BinaryIO) -> dict[str, Any]:
     reader = fastavro.reader(fo)
     assert isinstance(reader.writer_schema, dict)
     return reader.writer_schema
-
-
-def extract_alert(
-    candid: int, block: BinaryIO, schema: dict, codec: str = "null"
-) -> dict[str, Any]:
-    """
-    Extract single record from Avro block
-    """
-    read_block = BLOCK_READERS.get(codec)
-    if read_block is None:
-        raise KeyError(f"Unknown codec {codec}")
-
-    decoder = BinaryDecoder(block)
-    # consume record count to advance to the compressed block
-    nrecords = decoder.read_long()
-    assert nrecords > 0
-    # consume compressed block
-    buf = read_block(decoder)
-    # iterate over deserialized records
-    for _ in range(nrecords):
-        alert = fastavro.schemaless_reader(buf, get_parsed_schema(schema), None)
-        if isinstance(alert, dict) and alert["candid"] == candid:
-            return alert
-    raise KeyError(f"{candid} not found in block")
