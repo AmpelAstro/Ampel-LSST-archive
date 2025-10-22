d821ced4de0956c9ee8a6280aa569e9e5ff383a4
Jakob van Santen <jvansanten@gmail.com>
2025-10-22 18:17:02 +0200

    Fancy cutout plots

--- ampel/lsst/archive/server/cutouts.py
+++ ampel/lsst/archive/server/cutouts.py
@@ -1,11 +1,16 @@
 import io
+from typing import TypedDict
 
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
+from astropy.modeling import fitting, models
 from astropy.wcs import WCS
 from matplotlib.colors import Normalize
 from matplotlib.figure import Figure
+from matplotlib.offsetbox import AnchoredText
+from matplotlib.patches import Ellipse
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 from scipy import ndimage
 
 
@@ -53,6 +58,127 @@ def get_image_north_up_east_left(cutout_data: bytes) -> np.ndarray:
     x_rot = x_rot + crpix1
     y_rot = y_rot + crpix2
     return ndimage.map_coordinates(data, [y_rot, x_rot])
+
+
+class EllipseParams(TypedDict):
+    xy: tuple[float, float]
+    width: float
+    height: float
+    angle: float
+
+
+# factor to convert sigma to FWHM ~ 2.355
+FWHM = 2 * np.sqrt(2 * np.log(2))
+
+
+def get_halfmax_ellipse(flux: fits.PrimaryHDU, psf: fits.ImageHDU) -> EllipseParams:
+    """
+    Extract PSF from cutout HDUList. Returns parameters for
+    matplotlib.patches.Ellipse representing the half-maximum contour.
+    """
+    psf_data = psf.data
+
+    # guess mean from peak position
+    y0, x0 = np.unravel_index(np.argmax(psf_data), psf_data.shape)
+    # guess standard deviation from peak amplitude, assuming it's a gaussian
+    s0 = 1 / np.sqrt(2 * np.pi * psf_data.max())
+    a0 = psf_data.max()
+    p_init = models.Gaussian2D(
+        amplitude=a0,
+        x_mean=x0,
+        y_mean=y0,
+        x_stddev=s0,
+        y_stddev=s0,
+        theta=0,
+    )
+    y, x = np.mgrid[: psf_data.shape[0], : psf_data.shape[1]]
+    fit_p = fitting.TRFLSQFitter()(p_init, x, y, psf_data)
+    # find offset of center from cutout center
+    dx = fit_p.x_mean.value - psf_data.shape[0] // 2
+    dy = fit_p.y_mean.value - psf_data.shape[1] // 2
+
+    # reference coordinates of the cutout
+    # NB: pixel coordinates are 1-indexed in FITS
+    crpix = WCS(flux.header).wcs.crpix - 1
+
+    return {
+        # center on reference point of the cutout
+        "xy": (crpix[0] + dx, crpix[1] + dy),
+        "width": (FWHM * fit_p.x_stddev.value).tolist(),
+        "height": (FWHM * fit_p.y_stddev.value).tolist(),
+        "angle": np.degrees(fit_p.theta.value).tolist(),
+    }
+
+
+def make_cutout_plots(cutouts: dict[str, bytes]):
+    shrink = 1 / 1.2
+
+    fig, axes = plt.subplots(
+        1,
+        len(cutouts),
+        figsize=(5.0 * len(cutouts) * shrink, 5.0),
+        gridspec_kw={"wspace": 0.05},
+        layout="constrained",
+    )
+    show_scale_bar = True
+    for (k, v), ax in zip(cutouts.items(), axes, strict=False):
+        hdus = fits.open(io.BytesIO(v))
+        img = hdus[0].data
+
+        mask = np.isfinite(img)
+        axes_image = ax.imshow(
+            img,
+            origin="lower",
+            cmap="viridis",
+            # clip pixel values below the median
+            norm=Normalize(*np.percentile(img[mask], [0.5, 99.5])),
+        )
+        ax.set_axis_off()
+
+        # add PSF half-maximum ellipse
+        ellipse_params = get_halfmax_ellipse(hdus[0], hdus[2])
+        ellipse = Ellipse(
+            **ellipse_params, edgecolor="white", facecolor="none", lw=1, ls="--"
+        )
+        ax.add_patch(ellipse)
+
+        # add scale bar
+        # CDELT1 and CDELT2 are set to 1, but https://rubinobservatory.org/for-scientists/rubin-101/key-numbers says 0.2 arcsec/pixel
+        if show_scale_bar:
+            show_scale_bar = False
+            ax.add_artist(
+                AnchoredSizeBar(
+                    ax.transData,
+                    1 / 0.2,  # 1 arcsec in pixels
+                    "1''",
+                    "lower right",
+                    pad=0.5,
+                    frameon=False,
+                    sep=4,
+                    color="white",
+                )
+            )
+        # label cutout type
+        text = AnchoredText(k, loc="upper left", prop=dict(color="white"))
+        text.patch.set_facecolor(axes_image.get_cmap()(0))
+        text.patch.set_edgecolor("none")
+        text.patch.set_boxstyle("round,pad=0.1,rounding_size=0.2")
+        ax.add_artist(text)
+
+        # add colorbar
+        plt.colorbar(axes_image, orientation="horizontal", label="flux (nJy)", shrink=1)
+    return fig
+
+
+def render_cutout_plots(fits: dict[str, bytes]) -> str:
+    fig = make_cutout_plots(fits)
+    try:
+        with io.BytesIO() as buf:
+            fig.savefig(buf, format="svg")
+            return buf.getvalue().decode()
+    finally:
+        fig.clear()
+        plt.close(fig)
 
 
 def fits_to_png(cutout_data: bytes) -> bytes:
