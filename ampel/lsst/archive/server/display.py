0b332325cda4a1f62723bc62413063316b162cf7
Jakob van Santen <jvansanten@gmail.com>
2025-10-23 09:28:54 +0200

    Cutout roulette

--- ampel/lsst/archive/server/display.py
+++ ampel/lsst/archive/server/display.py
@@ -1,10 +1,14 @@
 from fastapi import (
     APIRouter,
+    status,
 )
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse, RedirectResponse
+from sqlalchemy import text
 
 from .alert import AlertFromId
 from .cutouts import render_cutout_plots
+from .db import AsyncSession
+from .settings import settings
 
 router = APIRouter(tags=["display"])
 
@@ -21,4 +25,20 @@ def display_cutouts(alert: AlertFromId):
             }
         ),
         media_type="image/svg+xml",
+    )
+
+
+@router.get("/roulette")
+async def rien_de_la_plus(
+    session: AsyncSession,
+):
+    """
+    Redirect to a (somewhat) random alert cutout page, just for fun.
+    """
+    diaSourceId = await session.scalar(
+        text("select id from alert TABLESAMPLE system_rows(1)")
+    )
+    return RedirectResponse(
+        url=f"{settings.root_path}/display/alert/{diaSourceId}/cutouts",
+        status_code=status.HTTP_303_SEE_OTHER,
     )
