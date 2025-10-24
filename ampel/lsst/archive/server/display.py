cab521dd51a2df3c6cbe2b59d46f9239d062dc2b
Jakob van Santen <jvansanten@gmail.com>
2025-10-24 13:45:03 +0200

    wip: alerts for object endpoint

--- ampel/lsst/archive/server/display.py
+++ ampel/lsst/archive/server/display.py
@@ -1,4 +1,6 @@
-from typing import Annotated
+import io
+from collections.abc import Sequence
+from typing import Annotated, cast
 
 from fastapi import (
     APIRouter,
@@ -7,10 +9,16 @@ from fastapi import (
 )
 from fastapi.responses import PlainTextResponse, RedirectResponse
 from sqlalchemy import text
+from sqlalchemy.sql.elements import ColumnElement
 
+from ..alert_packet import Alert as LSSTAlert
+from ..avro import extract_record
+from ..db import get_blobs_with_condition, get_schema
+from ..models import Alert
 from .alert import AlertFromId
 from .cutouts import render_cutout_plots
 from .db import AsyncSession
+from .s3 import Bucket, get_range
 from .settings import settings
 
 router = APIRouter(tags=["display"])
@@ -48,3 +56,45 @@ async def rien_de_la_plus(
         url=f"{settings.root_path}/display/alert/{diaSourceId}/cutouts?sigma=3",
         status_code=status.HTTP_303_SEE_OTHER,
     )
+
+
+async def get_alerts_with_condition(
+    session: AsyncSession,
+    bucket: Bucket,
+    conditions: "Sequence[ColumnElement[bool] | bool]",
+):
+    async for uri, start, end, schema_id in get_blobs_with_condition(
+        session, conditions
+    ):
+        schema = await get_schema(session, schema_id)
+        body = await get_range(bucket, uri, start, end)
+        record = extract_record(io.BytesIO(await body.read()), schema)
+        yield cast(LSSTAlert, record)
+
+
+@router.get("/diaobject/{diaObjectId}")
+async def get_alerts_for_diaobject(
+    diaObjectId: int,
+    session: AsyncSession,
+    bucket: Bucket,
+):
+    return [
+        alert
+        async for alert in get_alerts_with_condition(
+            session, bucket, [Alert.diaobject_id == diaObjectId]
+        )
+    ]
+
+
+@router.get("/ssobject/{ssObjectId}")
+async def get_alerts_for_ssobject(
+    ssObjectId: int,
+    session: AsyncSession,
+    bucket: Bucket,
+):
+    return [
+        alert
+        async for alert in get_alerts_with_condition(
+            session, bucket, [Alert.ssobject_id == ssObjectId]
+        )
+    ]
