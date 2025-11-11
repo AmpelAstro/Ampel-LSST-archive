cff5c5a154eb2331741387d34fcd2b618b6e0716
Jakob van Santen <jvansanten@gmail.com>
2025-11-11 12:06:16 +0100

    use public url in router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/alert/:diaSourceId" element={<AlertView />} />
         <Route path="/diaobject/:diaObjectId" element={<DIAObjectView />} />
